06c7f6ca395af1b249794318a352f85815a41f95
Sonic-Haliza <74656879+Sonic-Haliza@users.noreply.github.com>
2022-07-18 22:47:20 +0800

    Update jb-dana.js

--- plugins/jb-dana.js
+++ plugins/jb-dana.js
@@ -5,18 +5,9 @@ HayðŸ‘‹, ingin melanjutkan pembayaran?
 
 💌 *Pembayaran*
 💳 Via: Dana
-📞 Nomor: 088215689772
-👤 A/n: Fangz
-📦 Mitra: Fangz BOT
+📞 Nomor: 082211991425
+👤 A/n: Brynzz
 💰 Metode pembayaran: Online ( ~Cod~ )
-
-📣 *Beli*
-
-Apk TokoBot: https://app.bukaolshop.com/toko/tokobot
-
-Apk FangzApp:https://web.jagel.id/store/fangzapp
-
-Toko Website:https://toko.ly/TokoBot   (Non apk)
 
 ✏️ _Informasi Pembayaran_
 
@@ -27,7 +18,7 @@ Pastikan saldo kamu mencukupi untuk bertransaksi!
 ❗KLIK *SUDAH BAYAR* JIKA SUDAH MEMBAYAR!
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'QRIS', url: 'https://telegra.ph/file/ff71853efeb006d635a90.jpg'}},
+    {index: 1, urlButton: {displayText: 'QR', url: 'https://telegra.ph/file/ff71853efeb006d635a90.jpg'}},
     {index: 4, quickReplyButton: {displayText: 'Sudah membayar', id: '.sudahbayar'}},
 ]
 let tm = {
