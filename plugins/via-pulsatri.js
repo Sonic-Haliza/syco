c7c9ff2482a3352c8a2423dcfb98caa287dc350f
Sonic-Haliza <74656879+Sonic-Haliza@users.noreply.github.com>
2022-07-18 22:25:39 +0800

    Add files via upload

---
+++ plugins/via-pulsatri.js
@@ -0,0 +1,32 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let text = `${htki} TRI3 ${htka}
+
+Hay👋‹, ingin Donasi?
+
+💌 *Pembayaran*
+💳 Via: Pulsa Telkomsel
+📞 Nomor: 082211991425
+👤 A/n: Brynzx
+📝 Mitra: Sycoryz BOT
+💰 Metode pembayaran: Online ( ~Cod~ )
+
+
+®KLIK *SUDAH BAYAR* JIKA SUDAH TRANSFER 
+`
+const templateButtons = [
+    {index: 4, quickReplyButton: {displayText: 'Sudah membayar', id: '.sudahbayar'}},
+]
+let tm = {
+text: text,
+footer: global.wm,
+templateButtons: templateButtons,
+image: {url: fla + 'Donasi'}
+}
+conn.sendMessage(m.chat, tm, m)
+}
+handler.help = ['tri']
+handler.tags = ['info']
+handler.command = /^(donasitri)$/i
+handler.private = true
+
+export default handler
